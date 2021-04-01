2f6a27720dbdf77ae92e420424ddb31afa8cc7fb
Matthew Crompton <crompton.ml@gmail.com>
2021-04-01 19:54:03 +0200

    Fix header scrolling transition bug

--- src/Pages/Home/Header/Header.tsx
+++ src/Pages/Home/Header/Header.tsx
@@ -1,21 +1,23 @@
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
 import "./styles/styles.css";
 
 const Header = () => {
  const [isVisible, setIsVisible] = useState(true);
-
  useScrollPosition(
   ({ prevPos, currPos }) => {
    setIsVisible(currPos.y > prevPos.y);
   },
   [isVisible],
+  undefined,
+  undefined,
+  100,
  );
 
  return (
-  <header className={isVisible ? "header" : "hidden"}>
+  <header className={isVisible ? "header" : "header--hidden"}>
    <Logo />
    <button className="navbar__menu-toggle">toggle menu</button>
    <NavBar />
