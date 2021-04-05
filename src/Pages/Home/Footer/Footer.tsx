83c85a92ea5d3a2a50db866a7fac142fc0910f66
Matthew Crompton <crompton.ml@gmail.com>
2021-04-05 12:22:44 +0200

    fix date

--- src/Pages/Home/Footer/Footer.tsx
+++ src/Pages/Home/Footer/Footer.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
  return (
   <footer>
    <small>
-    &copy; Copyright new {new Date().getFullYear()}, Matthew Crompton &mdash;
-    Link to GitHub Repo
+    &copy; Copyright {new Date().getFullYear()}, Matthew Crompton &mdash; Link
+    to GitHub Repo
    </small>
   </footer>
  );
