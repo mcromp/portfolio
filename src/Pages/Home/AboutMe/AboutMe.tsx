842b725492cba060bafbec0edd51d1761db4157a
Matthew Crompton <crompton.ml@gmail.com>
2021-04-07 09:53:40 +0200

    Add styling for about me page

--- src/Pages/Home/AboutMe/AboutMe.tsx
+++ src/Pages/Home/AboutMe/AboutMe.tsx
@@ -1,4 +1,5 @@
 import "./styles/styles.css";
+import Emoji from "a11y-react-emoji";
 
 const AboutMe = () => {
  return (
@@ -9,29 +10,39 @@ const AboutMe = () => {
     <br />
     <br />
     If I had to describe myself in a word, I’d say ‘<u>maker</u>’.
-    <br />I have a natural motivation to learn, grow, tinker, and create.
-   </p>
-   <p>
-    As for programming...
-    <br />I first fell in love as a kid making text adventures with QBasic using
-    the family’s Windows 95 computer.
-   </p>
-   <p>
-    Cut to the present day, I’ve graduated from online front end courses and
+    <br />
+    I have a natural motivation to learn, grow, tinker, and create.
+    <br />
+    <br />
+    As for programming... <br />
+    I first fell in love as a kid making text adventures with QBasic using the
+    family’s Windows 95 computer.
+    <br />
+    <br />
+    Cut to the present day, I’ve completed several online frontend courses and
     successfully deployed several web apps (none of which are set in a dungeon,
     unfortunately).
     <br />
+    <br />
     Outside of programming I enjoy creating art, traveling, playing guitar, and
-    hiking. ...Yes, also coffee.
+    hiking.
+    <br />
+    ...Yes, also coffee
+    <Emoji symbol="☕" label="coffee" />
    </p>
    <p>Some of the tech I've been using recently:</p>
-   <ul>
-    <li>React</li>
-    <li>Redux</li>
-    <li>JavaScript (ES6+)</li>
-    <li>TypeScript</li>
-    <li></li>
-   </ul>
+   <div className="about-me__tech-list">
+    <ul>
+     <li>React</li>
+     <li>Redux</li>
+     <li>JavaScript (ES6+)</li>
+    </ul>
+    <ul>
+     <li>TypeScript</li>
+     <li>SASS/CSS3</li>
+     <li>NodeJS</li>
+    </ul>
+   </div>
   </section>
  );
 };
