5a773fff4fec14db66895247d4ad7fd7809d145a
Matthew Crompton <crompton.ml@gmail.com>
2021-04-07 09:22:16 +0200

    Add style to about me section

--- src/Pages/Home/AboutMe/AboutMe.tsx
+++ src/Pages/Home/AboutMe/AboutMe.tsx
@@ -2,20 +2,37 @@ import "./styles/styles.css";
 
 const AboutMe = () => {
  return (
-  <div id="about" className="about-me">
-   About Measldjf ;lkjasd f;lkajs fd;lksjfAbout Measldjf ;lkjasd f;lkajs
-   fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd
-   f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf
-   ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf About
-   Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf
-   About Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs
-   fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd
-   f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf
-   ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf About
-   Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf
-   About Measldjf ;lkjasd f;lkajs fd;lksjf About Measldjf ;lkjasd f;lkajs
-   fd;lksjf About Measldjf ;lkjasd f;lkajs fd;lksjf
-  </div>
+  <section id="about" className="about-me">
+   <h2 className="section_title">About</h2>
+   <p>
+    Hey, thanks for stopping by!
+    <br />
+    <br />
+    If I had to describe myself in a word, I’d say ‘<u>maker</u>’.
+    <br />I have a natural motivation to learn, grow, tinker, and create.
+   </p>
+   <p>
+    As for programming...
+    <br />I first fell in love as a kid making text adventures with QBasic using
+    the family’s Windows 95 computer.
+   </p>
+   <p>
+    Cut to the present day, I’ve graduated from online front end courses and
+    successfully deployed several web apps (none of which are set in a dungeon,
+    unfortunately).
+    <br />
+    Outside of programming I enjoy creating art, traveling, playing guitar, and
+    hiking. ...Yes, also coffee.
+   </p>
+   <p>Some of the tech I've been using recently:</p>
+   <ul>
+    <li>React</li>
+    <li>Redux</li>
+    <li>JavaScript (ES6+)</li>
+    <li>TypeScript</li>
+    <li></li>
+   </ul>
+  </section>
  );
 };
 
