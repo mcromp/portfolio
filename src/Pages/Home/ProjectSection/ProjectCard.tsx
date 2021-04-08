4446ba6565be60fb1d270b141478917316f064d4
Matthew Crompton <crompton.ml@gmail.com>
2021-04-08 17:40:08 +0200

    Add emoji icons to project cards

--- src/Pages/Home/ProjectSection/ProjectCard.tsx
+++ src/Pages/Home/ProjectSection/ProjectCard.tsx
@@ -6,8 +6,8 @@ import "./styles/styles.css";
 const ProjectCard: React.FC<any> = ({ title, to, icon, text }) => {
  return (
   <Link className="proj-card" to={`/p/${to}`}>
-   <ProjectIcon icon={icon} />
    <h4 className="proj-card__title">{title}</h4>
+   <ProjectIcon icon={icon} />
    <p className="proj-card__text">{text}</p>
   </Link>
  );
@@ -15,7 +15,9 @@ const ProjectCard: React.FC<any> = ({ title, to, icon, text }) => {
 
 const ProjectIcon = ({ icon }: any) => {
  if (icon.emoji) {
-  return <Emoji symbol={icon.emoji} label={icon.label} />;
+  return (
+   <Emoji className="proj-card__icon" symbol={icon.emoji} label={icon.label} />
+  );
  }
  return <p></p>;
 };
