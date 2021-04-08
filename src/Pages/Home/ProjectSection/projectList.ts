df7fff6bfde9264b0fa07472d5f173e94a63f808
Matthew Crompton <crompton.ml@gmail.com>
2021-04-08 14:59:07 +0200

    Add project screenshots

--- src/Pages/Home/ProjectSection/projectList.ts
+++ src/Pages/Home/ProjectSection/projectList.ts
@@ -1,22 +1,26 @@
+import flag from "../../../Assets/flag.png";
+import menu from "../../../Assets/menu.png";
+import muz from "../../../Assets/muzika.png";
+
 export const projectList: any = [
  {
   to: "menu",
   title: "Munu Plan",
-  img: "",
+  img: menu,
   text:
    "Web application that generates a printable 7-day food calendar, letting users select food items from a customizable list of food.",
  },
  {
   to: "muz",
   title: "MuziKa!",
-  img: "",
+  img: muz,
   text:
    "Musical ear-training web application in which users guess the pitch of a musical note after a chord progression is played.",
  },
  {
   to: "flag",
   title: "Flag Memeory",
-  img: "",
+  img: flag,
   text:
    "A memory game web application that reveals the wikipedia summary of a country when its flags are correctly matched",
  },
