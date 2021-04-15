a1cd114d4811d98689618a0adf89c0386b1bbe30
Matthew Crompton <crompton.ml@gmail.com>
2021-04-15 11:26:37 +0200

    Change name of pages to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Switch, Route } from "react-router-dom";
-import AboutMe from "./Pages/Home/AboutMe/AboutMe";
-import Contact from "./Pages/Home/Contact/Contact";
+import AboutMe from "./Pages/App/AboutMe/AboutMe";
+import Contact from "./Pages/App/Contact/Contact";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
-import Landing from "./Pages/Home/Landing/Landing";
-import Projects from "./Pages/Home/Projects/Projects";
+import Landing from "./Pages/App/Landing/Landing";
+import Projects from "./Pages/App/Projects/Projects";
 import ScrollToTop from "./Pages/Shared/ScrollToTop/ScrollToTop";
 import "./App.css";
 
