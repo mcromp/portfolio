170f7ae7295708199da268f3c7a14c517316c153
Matthew Crompton <crompton.ml@gmail.com>
2021-04-16 13:44:09 +0200

    Use react responsive to add dynamic 3d head size

--- src/App/Landing/Head3D.tsx
+++ src/App/Landing/Head3D.tsx
@@ -1,28 +1,35 @@
 import { MapControls, Stars, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 import Head3DModel from "../../Assets/Head3DModel";
 
 const Head3D = () => {
+ const isBigScreen = useMediaQuery({ query: "(min-width: 600px)" });
  return (
-  <Canvas className="head3d">
-   <Suspense
-    fallback={
-     <Text color="black" fontSize={3}>
-      Loading...
-     </Text>
-    }
+  <div className="head3d">
+   <Canvas
+    className="canvas"
+    style={{ height: isBigScreen ? "100vh" : "200px" }}
    >
-    <MapControls enableZoom={false} />
-    <ambientLight intensity={0.5} />
-    <spotLight position={[100, 15, 10]} angle={0.3} />
-    <Stars factor={10} saturation={0} fade />
-    <Head3DModel position={[0, 0, 0]} scale={[20, 20, 20]} />
-    <Text color="black" fontSize={0.3}>
-     Oh Hi! How are you?
-    </Text>
-   </Suspense>
-  </Canvas>
+    <Suspense
+     fallback={
+      <Text color="black" fontSize={3}>
+       Loading...
+      </Text>
+     }
+    >
+     <MapControls enableZoom={false} />
+     <ambientLight intensity={0.5} />
+     <spotLight position={[100, 15, 10]} angle={0.3} />
+     <Stars factor={10} saturation={0} fade />
+     <Head3DModel position={[0, 0, 0]} scale={[20, 20, 20]} />
+     <Text color="#222" fontSize={0.3}>
+      Oh Hi! How are you?
+     </Text>
+    </Suspense>
+   </Canvas>
+  </div>
  );
 };
 
