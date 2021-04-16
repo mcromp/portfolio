8292be47f1538c9953b5fd8d4bc723cc06e1089c
Matthew Crompton <crompton.ml@gmail.com>
2021-04-16 14:31:29 +0200

    Refactor components out of shared folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route } from "react-router-dom";
 import AboutMe from "./App/AboutMe/AboutMe";
-import ScrollToTop from "./App/Shared/ScrollToTop/ScrollToTop";
+import ScrollToTop from "./App/ScrollToTop/ScrollToTop";
 import Contact from "./App/Contact/Contact";
 import Landing from "./App/Landing/Landing";
 import Projects from "./App/Projects/Projects";
-import Footer from "./App/Shared/Footer/Footer";
-import Header from "./App/Shared/Header/Header";
+import Footer from "./App/Footer/Footer";
+import Header from "./App/Header/Header";
 import "./App.css";
 
 function App() {
