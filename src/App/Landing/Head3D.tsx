a5cace76c38de0ae0f321b7526f326888be8ae5b
Matthew Crompton <crompton.ml@gmail.com>
2021-04-16 14:40:29 +0200

    Restore accidently deleted files

--- src/App/Landing/Head3D.tsx
+++ src/App/Landing/Head3D.tsx
@@ -2,7 +2,7 @@ import { MapControls, Stars, Text } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { useMediaQuery } from "react-responsive";
-import Head3DModel from "../../Assets/Head3DModel";
+// import Head3DModel from "../../Asset";
 
 const Head3D = (): JSX.Element => {
  const isBigScreen = useMediaQuery({ query: "(min-width: 600px)" });
@@ -17,7 +17,7 @@ const Head3D = (): JSX.Element => {
      <ambientLight intensity={0.5} />
      <spotLight position={[100, 15, 10]} angle={0.3} />
      <Stars factor={10} saturation={0} fade />
-     <Head3DModel position={[0, 0, 0]} scale={[20, 20, 20]} />
+     {/* <Head3DModel position={[0, 0, 0]} scale={[20, 20, 20]} /> */}
      <Text color="#222" fontSize={0.3}>
       Oh Hi! How are you?
      </Text>
