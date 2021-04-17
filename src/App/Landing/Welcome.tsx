cd1888d6fd4b606b99c9f616b6edea316da7c9cd
Matthew Crompton <crompton.ml@gmail.com>
2021-04-17 11:43:10 +0200

    Add gsap animation

--- src/App/Landing/Welcome.tsx
+++ src/App/Landing/Welcome.tsx
@@ -1,6 +1,23 @@
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
 const Welcome = (): JSX.Element => {
+ let content = useRef<HTMLDivElement>(null);
+
+ useEffect(() => {
+  if (content) {
+   gsap.to(content.current, 0, { css: { visibility: "visible" } });
+   gsap.from(content.current, {
+    autoAlpha: 0,
+    ease: "bounce",
+    delay: 0.5,
+   });
+   gsap.from(content.current, { scale: 0.1, duration: 1 });
+  }
+ }, []);
+
  return (
-  <div className="welcome">
+  <div className="welcome" ref={content}>
    <h3 className="welcome__title">
     Matthew <br />
     Crompton
