9fa09690df5498ecda9228ae24d4601480099917
Matthew Crompton <crompton.ml@gmail.com>
2021-04-23 14:11:02 +0200

    Add content email address

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,3 +1,5 @@
+import { Mail } from "react-feather";
+import { IconLink } from "../../StyleComponents/Block/";
 import "./styles/styles.css";
 
 const Contact = (): JSX.Element => {
@@ -7,9 +9,12 @@ const Contact = (): JSX.Element => {
     Contact
    </h2>
    <div>
-    {/* <a className="contact" href="mailto:mcromp.js@gmail.com?subject=Hello!">
-     MCROMP.JS@GMAIL.COM
-    </a> */}
+    <p className="email-text">mcromp.js@gmail.com</p>
+    <div className="email-icon">
+     <IconLink url="mailto:mcromp.js@gmail.com?subject=Hello!">
+      <Mail size={50} color="black" className="block__icon-link" />
+     </IconLink>
+    </div>
    </div>
   </div>
  );
