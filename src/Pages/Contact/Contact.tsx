0d8098183cd0fb6cf2dfd54805e231f3077fb3d7
Matthew Crompton <crompton.ml@gmail.com>
2021-04-23 16:49:14 +0200

    Correct stylings for contact title

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,16 +1,14 @@
 import { Mail } from "react-feather";
 import { IconLink } from "../../StyleComponents/Block/";
 import "./styles/styles.css";
-
+import * as Section from "../../StyleComponents/Section";
 const Contact = (): JSX.Element => {
  return (
-  <div className="contact">
-   <h2 id="contact" className="section_title">
-    Contact
-   </h2>
-   <div>
-    <p className="email-text">mcromp.js@gmail.com</p>
-    <div className="email-icon">
+  <div className="contact" id="contact">
+   <Section.Title>Contact</Section.Title>
+   <div className="email">
+    <p className="email__text">mcromp.js@gmail.com</p>
+    <div className="email__icon">
      <IconLink url="mailto:mcromp.js@gmail.com?subject=Hello!">
       <Mail size={50} color="black" className="block__icon-link" />
      </IconLink>
