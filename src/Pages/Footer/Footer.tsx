29df8222764d3b17ba3fddf5800a9f7316565fea
Matthew Crompton <crompton.ml@gmail.com>
2021-04-23 13:56:23 +0200

    Style footer

--- src/Pages/Footer/Footer.tsx
+++ src/Pages/Footer/Footer.tsx
@@ -1,10 +1,12 @@
+import Emoji from "a11y-react-emoji";
 import "./styles/styles.css";
 
 const Footer = (): JSX.Element => {
  return (
   <footer className="footer">
    <small className="footer__text">
-    Designed and built from scratch by Matthew Crompton &mdash;{" "}
+    Built with <Emoji symbol="💓" label="love" /> from scratch by Matthew
+    Crompton &mdash;{" "}
     <a href="https://github.com/mcromp/portfolio">
      check out the repo to learn more!
     </a>
