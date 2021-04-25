0857f550c7bd7cd7cf145a2bdfe5bde96a034846
Matthew Crompton <crompton.ml@gmail.com>
2021-04-25 16:18:16 +0200

    Add photo

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,17 +1,21 @@
 import { Mail } from "react-feather";
 import { IconLink } from "../../StyleComponents/Block/";
-import "./styles/styles.css";
 import * as Section from "../../StyleComponents/Section";
+import self from "../../Assets/self.jpeg";
+import "./styles/styles.css";
 const Contact = (): JSX.Element => {
  return (
   <div className="contact" id="contact">
    <Section.Title>Contact</Section.Title>
-   <div className="email">
-    <p className="email__text">mcromp.js@gmail.com</p>
-    <div className="email__icon">
-     <IconLink url="mailto:mcromp.js@gmail.com?subject=Hello!">
-      <Mail size={50} color="black" className="block__icon-link" />
-     </IconLink>
+   <div className="flex">
+    <img src={self} alt="phot of matthew crompton" className="self-img" />
+    <div className="email">
+     <p className="email__text">mcromp.js@gmail.com</p>
+     <div className="email__icon">
+      <IconLink url="mailto:mcromp.js@gmail.com?subject=Hello!">
+       <Mail size={50} color="black" className="block__icon-link" />
+      </IconLink>
+     </div>
     </div>
    </div>
   </div>
